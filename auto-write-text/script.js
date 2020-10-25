d320fb15970cc2c53c27df9ef786d95b4d57ca3a
Scott <kongsa0419@gmail.com>
2020-10-25 11:45:52 +0900

    3

--- auto-write-text/script.js
+++ auto-write-text/script.js
@@ -2,17 +2,11 @@ const text = "I deserve respect!!";
 
 let idx = 0;
 
-// function writeText(){
-//     document.body.innerText  = text.slice(0, idx);
-//     idx++;
-//     if(idx > text.length() - 1){
-//         idx = 0;
-//     }
-// }
-setInterval(function writeText(){
+function writeText(){
     document.body.innerText  = text.slice(0, idx);
     idx++;
     if(idx > text.length() - 1){
         idx = 0;
     }
-}, 100);
+}
+setInterval(writeText, 100);
